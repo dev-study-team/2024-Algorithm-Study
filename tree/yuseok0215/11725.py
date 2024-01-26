7a1e42e539d784cab3f341ff769ab3ed6906887b
yuseok0215 <elwlahstmxjf@naver.com>
2024-01-26 17:19:51 +0900

    2024.01.26 tree

---
+++ tree/yuseok0215/11725.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n = int(input())
+
+tree = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a,b = map(int, input().split())
+
+    tree[a].append(b)
+    tree[b].append(a)
+    
+parent = [0] * (n+1)
+
+def bfs():
+    q = deque()
+    q.append(1)
+
+    while q:
+        x = q.popleft()
+
+        for nx in tree[x]:
+            if parent[nx] == 0:
+                parent[nx] = x
+                q.append(nx)
+        
+bfs()
+
+for i in range(1,n+1):
+    print(parent[i])
+
